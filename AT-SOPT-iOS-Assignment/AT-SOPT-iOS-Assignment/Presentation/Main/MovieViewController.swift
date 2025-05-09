99247fea1b2ff988bdd9ae1afcb64ac522397b39
KooWihC <ahnchiwook@hanmail.net>
2025-05-09 16:44:20 +0900

    [Feat] APIModel 생성

---
+++ AT-SOPT-iOS-Assignment/AT-SOPT-iOS-Assignment/Presentation/Main/MovieViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MovieViewController.swift
+//  AT-SOPT-iOS-Assignment
+//
+//  Created by 안치욱 on 5/9/25.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class MovieViewController: UIViewController {
+    
+    
+    
+}
