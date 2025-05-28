8c33005de8c5a7074d1b0b611116cfd0a90897a2
KooWihC <ahnchiwook@hanmail.net>
2025-05-28 18:22:26 +0900

    [Add] Add Assets

---
+++ TVING_SwiftUI/TVING_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TVING_SwiftUI
+//
+//  Created by 안치욱 on 5/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TVING_SwiftUI/TVING_SwiftUI/TVING_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVING_SwiftUIApp.swift
+//  TVING_SwiftUI
+//
+//  Created by 안치욱 on 5/28/25.
+//
+
+import SwiftUI
+
+@main
+struct TVING_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
