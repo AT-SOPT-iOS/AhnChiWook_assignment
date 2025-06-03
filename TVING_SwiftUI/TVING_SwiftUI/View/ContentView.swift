841358d4a5b6378f2d5c9e64481cc84c2603c67f
KooWihC <ahnchiwook@hanmail.net>
2025-06-03 22:15:51 +0900

    [Feat] BottomView 만들기

--- TVING_SwiftUI/TVING_SwiftUI/View/ContentView.swift
+++ TVING_SwiftUI/TVING_SwiftUI/View/ContentView.swift
@@ -20,6 +20,8 @@ struct ContentView: View {
                 TeamListView()
                 Spacer()
                 ServiceListView()
+                Top6View()
+                BottomView()
             }
         }.background(Color.black)
     }
